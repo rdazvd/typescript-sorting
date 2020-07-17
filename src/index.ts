25fae74d8333310d43b2e8c1b6164fce217babda
Rafael de Azevedo <29780126+rdazvd@users.noreply.github.com>
2020-07-17 16:13:47 -0300

    Feat: created Sorter class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('hi there');
+class Sorter {
+  collection: number[];
+
+  constructor(collection: number[]) {
+    this.collection = collection;
+  }
+
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHandSide = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHandSide;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
